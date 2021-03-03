41d1afb81253ffc172fe09b2f1001b46730044cf
Isamu Ito <isamsito@gmail.com>
2021-03-03 22:39:15 +0000

    add basic query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,3 +6,23 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
+pool.connect(()=>{console.log('connected')})
+
+const runQuery = (cohortName, numResults) => {
+  pool.query(`
+    SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort_name
+    FROM students
+    JOIN cohorts ON cohorts.id = students.cohort_id
+    WHERE cohorts.name LIKE '${cohortName}%'
+    LIMIT ${numResults || 5};
+  `)
+  .then(res => {
+    console.log(res.rows);
+  })
+  .catch(err => console.error('query error', err.stack));
+};
+
+const args = process.argv.slice(2);
+
+runQuery(args[0],args[1]);
